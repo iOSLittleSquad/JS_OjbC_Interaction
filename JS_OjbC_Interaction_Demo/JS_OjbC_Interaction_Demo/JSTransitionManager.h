648b3ad59e9dc2c3f6ca38c1093f6e6180887936
caolongyao <asus_cly@126.com>
2016-04-14 17:05:04 +0800

    test.

---
+++ JS_OjbC_Interaction_Demo/JS_OjbC_Interaction_Demo/JSTransitionManager.h
@@ -0,0 +1,13 @@
+//
+//  JSTransitionManager.h
+//  JS_OjbC_Interaction_Demo
+//
+//  Created by aa on 16/3/29.
+//  Copyright © 2016年 iOSLittleSquad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSTransitionManager : NSObject
+
+@end
---
+++ JS_OjbC_Interaction_Demo/JS_OjbC_Interaction_Demo/JSTransitionManager.m
@@ -0,0 +1,13 @@
+//
+//  JSTransitionManager.m
+//  JS_OjbC_Interaction_Demo
+//
+//  Created by aa on 16/3/29.
+//  Copyright © 2016年 iOSLittleSquad. All rights reserved.
+//
+
+#import "JSTransitionManager.h"
+
+@implementation JSTransitionManager
+
+@end
